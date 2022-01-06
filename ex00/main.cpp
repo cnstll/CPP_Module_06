04160d45c2eb3a58ebf11bd78e3d645ae0acd4a6
cnstll <calle@student.42.fr>
2022-01-06 18:10:51 +0100

    ex00, tester built

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,5 +1,11 @@
+#include <cstdlib>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    return (0);
+    if (argc != 2)
+        return (EXIT_FAILURE);
+    float f = atof(argv[1]);
+    std::cout << f << std::endl; 
+    return (EXIT_SUCCESS);
 }
---
+++ ex00/test.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+compile_program()
+{
+    make -C $1 re --silent
+    make -C $1 clean  --silent
+}
+
+printTestName()
+{
+    TEST_NAME=${1:1}
+    echo 
+    echo "--------------------------"
+    echo "         $TEST_NAME     "
+    echo "--------------------------"
+    echo
+}
+
+isTestName()
+{
+    LINE=$1
+    if [ "${LINE::1}" == "#" ]
+    then
+        return 1
+    else
+        return 0
+    fi
+}
+
+do_tests()
+{
+    PROGRAM_NAME=$1
+    TEST_FILE="./inputs_to_test"
+    TEST_NO=1
+    TOTAL_NUMBER_OF_TESTS=$(cat "$TEST_FILE" | wc -l)
+    while [ $TEST_NO -le $TOTAL_NUMBER_OF_TESTS ]
+    do
+        INPUT_TO_TEST=$(sed -n "${TEST_NO}p" $TEST_FILE)
+        isTestName "$INPUT_TO_TEST"
+        if [ $? -eq 1 ]
+        then
+            printTestName "$INPUT_TO_TEST"
+        else
+            echo -n "Test Line: $TEST_NO -- "
+            echo "Input: \""$INPUT_TO_TEST"\""
+            ./$PROGRAM_NAME "$INPUT_TO_TEST"
+        fi
+        TEST_NO=$((TEST_NO + 1))
+    done
+
+}
+
+PROGRAM_ROOT_DIR="./"
+PROGRAM_NAME="convert"
+
+compile_program $PROGRAM_ROOT_DIR
+chmod 755 $PROGRAM_NAME
+do_tests "$PROGRAM_NAME"
